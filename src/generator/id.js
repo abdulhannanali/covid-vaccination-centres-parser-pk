f7c1dd1005186cd88d6f22d88534a84012588149
Hannan Ali <abdulhannanali@users.noreply.com>
2021-09-07 06:05:18 +0500

    fix package json

--- src/generator/id.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var crypto_1 = require("crypto");
-function getId(name, district, tehsil) {
-    return crypto_1.createHash('sha256')
-        .update(name + district + tehsil)
-        .digest('hex');
-}
-exports["default"] = getId;
--- src/generator/localDebug.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var debug_1 = require("debug");
-exports["default"] = debug_1["default"]('centreParser:generator').extend;
